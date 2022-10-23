59cd7ecbbeda945501e19577960dc40c2d88e178
vclong2003 <vclong2003@gmail.com>
2022-10-23 22:26:31 +0700

    done MBTI point rule

--- src/Components/MBTI test/index.js
+++ src/Components/MBTI test/index.js
@@ -194,4 +194,359 @@ let points = [
       },
     ],
   },
+  {
+    group: "N",
+    ruleset: [
+      {
+        questionId: 2,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 5,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 10,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 12,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 15,
+        selection: "A",
+        point: 0,
+      },
+      {
+        questionId: 20,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 23,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 28,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 31,
+        selection: "A",
+        point: 0,
+      },
+      {
+        questionId: 35,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 38,
+        selection: "A",
+        point: 0,
+      },
+      {
+        questionId: 42,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 45,
+        selection: "A",
+        point: 0,
+      },
+      {
+        questionId: 48,
+        selection: "B",
+        point: 1,
+      },
+    ],
+  },
+  {
+    group: "T",
+    ruleset: [
+      {
+        questionId: 4,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 14,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 22,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 30,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 32,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 33,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 37,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 39,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 40,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 44,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 46,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 47,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 49,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 50,
+        selection: "A",
+        point: 2,
+      },
+    ],
+  },
+  {
+    group: "F",
+    ruleset: [
+      {
+        questionId: 4,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 14,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 22,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 30,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 32,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 33,
+        selection: "A",
+        point: 0,
+      },
+      {
+        questionId: 37,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 39,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 40,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 44,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 46,
+        selection: "B",
+        point: 0,
+      },
+      {
+        questionId: 47,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 49,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 50,
+        selection: "B",
+        point: 0,
+      },
+    ],
+  },
+  {
+    group: "J",
+    ruleset: [
+      {
+        questionId: 1,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 7,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 8,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 11,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 17,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 18,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 19,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 25,
+        selection: "A",
+        point: 1,
+      },
+      {
+        questionId: 25,
+        selection: "C",
+        point: 0,
+      },
+      {
+        questionId: 27,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 34,
+        selection: "A",
+        point: 2,
+      },
+      {
+        questionId: 41,
+        selection: "A",
+        point: 2,
+      },
+    ],
+  },
+  {
+    group: "P",
+    ruleset: [
+      {
+        questionId: 1,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 7,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 7,
+        selection: "C",
+        point: 1,
+      },
+      {
+        questionId: 8,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 11,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 17,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 18,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 19,
+        selection: "B",
+        point: 1,
+      },
+      {
+        questionId: 25,
+        selection: "B",
+        point: 0,
+      },
+      {
+        questionId: 27,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 34,
+        selection: "B",
+        point: 2,
+      },
+      {
+        questionId: 41,
+        selection: "B",
+        point: 2,
+      },
+    ],
+  },
 ];
