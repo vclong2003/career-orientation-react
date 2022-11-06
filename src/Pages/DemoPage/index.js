9ddf8f1d415a4bdd280c18cfa49694e7d76cf9b2
vclong2003 <vclong2003@gmail.com>
2022-11-07 02:45:36 +0700

    backup

--- src/Pages/DemoPage/index.js
+++ src/Pages/DemoPage/index.js
@@ -1,73 +1,16 @@
-import { Button, Container } from "react-bootstrap";
-import React, { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
+import { Container } from "react-bootstrap";
 import "react-quill/dist/quill.snow.css";
+import RichTextEditor from "../../Components/RichTextEditor";
 import styles from "./style.module.css";
-
-function MyComponent() {
-  const [value, setValue] = useState("");
-
-  useEffect(() => {
-    if (sessionStorage.getItem("para") != null) {
-      setValue(sessionStorage.getItem("para"));
-    }
-    console.log(value);
-  }, []);
-
-  return (
-    <Container>
-      <Button
-        onClick={() => {
-          sessionStorage.setItem("para", value);
-          setValue(value);
-        }}
-      >
-        Test
-      </Button>
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        modules={{
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ["bold", "italic", "underline", "strike", "blockquote"],
-            [
-              { list: "ordered" },
-              { list: "bullet" },
-              { indent: "-1" },
-              { indent: "+1" },
-            ],
-            ["link", "image"],
-            ["clean"],
-          ],
-        }}
-        formats={[
-          "header",
-          "bold",
-          "italic",
-          "underline",
-          "strike",
-          "blockquote",
-          "list",
-          "bullet",
-          "indent",
-          "link",
-          "image",
-        ]}
-      />
-      <Container
-        dangerouslySetInnerHTML={{ __html: value }}
-        className={styles.para}
-      ></Container>
-    </Container>
-  );
-}
 
 export default function DemoPage() {
   return (
     <Container>
-      <MyComponent />
+      <RichTextEditor
+        onChangeCallback={(value) => {
+          console.log(value);
+        }}
+      />
     </Container>
   );
 }
